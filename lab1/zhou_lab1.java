2bba08bbfcaee91754e44843f221ef0b2a1f5307
1o8ryan <1o8zhouryan@gmail.com>
2025-08-28 09:23:09 -0400

    finished everything

--- lab1/zhou_lab1.java
+++ lab1/zhou_lab1.java
@@ -10,30 +10,48 @@ class zhou_lab1{
         int tokenCount = 0;
         int intCount = 0;
         int intSum = 0;
+        int[] returnable = new int[0];
+        String[] input = new String[0];
         if (args.length == 0){
+            //when you forget to add the text file with data
             System.out.println("you forgor smth");
             System.exit(0);
         }
         else if(args.length > 1){
-            //something with a hashmap in orde rto store all the others
+            //when there is more than one arg which means I have to test the amount of the others there are.
+            //something with a hashmap or string[] in order to store all the others
+            returnable = new int[args.length - 1];
+            input = new String[args.length - 1];
+            for (int i = 1; i <args.length; i++){
+                returnable[i-1] = 0;
+                input[i-1] = args[i];
+            }
         }
         //scanner starts
         try{
             File myFile = new File(args[0]);
             Scanner reader = new Scanner(myFile);
             while(reader.hasNextLine()){
+                //loop through every line
                 lineCount++;
                 String lineAt = reader.nextLine();
                 String[] items = lineAt.split(" ");
                 for (int i = 0; i<items.length; i++){
+                    //loop through every token in the line
                     tokenCount++;
                     try{
+                        //test for int and then add them
                         Integer.parseInt(items[i]);
                         intCount++;
                         intSum = intSum + Integer.parseInt(items[i]);
                     }
                     catch(Exception e){
-
+                        //dont got anythign to throw here
+                    }
+                    for(int j = 0; j < input.length; i++){
+                        if (items[j].compareTo(input[j]) == 0){
+                            returnable[j] = returnable[j] + 1;
+                        }
                     }
                 }
             }
@@ -47,5 +65,8 @@ class zhou_lab1{
         System.out.println("tokenCount = " + tokenCount);
         System.out.println("intCount = " + intCount);
         System.out.println("intSum = " + intSum);
+        for(int i = 0; i < returnable.length; i++){
+            System.out.println("Amount of " + input[i] + " = " + returnable[i]); 
+        }
     }
 }
